c95eea63fcb780cf863d04f6f87d6a80d5969865
Kian William Nowrouzian <webarchitect@kwproductions121.com>
2021-03-31 10:38:51 +0430

    Update addarticletojshopping.php

--- addarticletojshopping.php
+++ addarticletojshopping.php
@@ -56,6 +56,7 @@ class PlgContentAddarticletojshopping extends CMSPlugin
          $itemrec = new stdClass();	
          $itemrec->product_id = 0;
          $itemrec->product_quantity = 1.00;
+        //pro-version feature
          $itemrec->unlimited = 1;
 		 $itemrec->product_ean = $article->id;
 		 $itemrec->manufacturer_code = $user->id;
